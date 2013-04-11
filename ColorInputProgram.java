0398b4c57135216daeec56285cfc0dddaea8430c
Nikhil Paranjape <nikhil1.paranjape@gmail.com>
2013-04-11 18:10:33 -0400

    New UI
    Icons added, better error screen, no crashing, recompiled new jar file.
    Enjoy :)

--- ColorInputProgram.java
+++ ColorInputProgram.java
@@ -1,10 +1,7 @@
 /**
  * Testing the values of red
  * @author Nikhil Paranjape 
- * @version March 2013 v2.0
- * DO NOT RERELEASE WITHOUT PERMISSION
- * you may use for guidance, education, and entertainment
- * feel free to add on, repost onto this repository
+ * @version March 2013
  */
 import java.applet.Applet;
 import java.awt.*;
@@ -16,37 +13,36 @@ import java.io.*;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
-public class ColorInputProgram
+import javax.swing.*;
+public class ColorInputProgram extends Object
 {
+    private static Icon icon, error;
     public static void main(String args[]){
-        String ColorInput;
-        UIManager UI=new UIManager();
-        
-        //Sets default window color
-        UI.put("OptionPane.background",new ColorUIResource(250,235,215));
-        UI.put("Panel.background",new ColorUIResource(250,235,215));
-        JOptionPane.showMessageDialog(null, "Welcome to nikhil's color converter");
-        ColorInput = JOptionPane.showInputDialog("Tell me a hex number color and I will make a color");
-        
-        //Converts hex into rgb so the computer can use it
-        Color color = Color.decode(ColorInput);
-        JOptionPane.showMessageDialog(null,"Your color awaits, press OK!");
-        
-        //Next method sets the color of the window
-        UI.put("OptionPane.background",new ColorUIResource(color));
-        UI.put("Panel.background",new ColorUIResource(color));
-        JOptionPane.showMessageDialog(null,"UR BRAND NEW COLOR","Color Window", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Check the next window for the rgb color code");
-        JOptionPane.showMessageDialog(null,color);
-        if(ColorInput.equalsIgnoreCase("#FF69B4")){
-            JOptionPane.showMessageDialog(null,"Hmm... Hotpink...");
+        try{
+            String ColorInput;
+            UIManager UI=new UIManager();
+            final ImageIcon icon = new ImageIcon("painticon.png");
+            final ImageIcon error = new ImageIcon("error.png");
+            
+            //sets default window color
+            UI.put("OptionPane.background",new ColorUIResource(250,235,215));
+            UI.put("Panel.background",new ColorUIResource(250,235,215));
+            JOptionPane.showMessageDialog(null, "Welcome to Nikhil's color converter", "Program", JOptionPane.INFORMATION_MESSAGE, icon);
+            ColorInput = JOptionPane.showInputDialog("Tell me a hex number color and I will make a color");
+            //Converts hex into rgb so the computer can use it
+            Color color = Color.decode(ColorInput);
+            JOptionPane.showMessageDialog(null,"Your color awaits, press OK!");
+            //NExt method sets the color of the window
+            UI.put("OptionPane.background",new ColorUIResource(color));
+            UI.put("Panel.background",new ColorUIResource(color));
+            JOptionPane.showMessageDialog(null,"UR BRAND NEW COLOR","Color Window", JOptionPane.INFORMATION_MESSAGE, icon);
+            UI.put("OptionPane.background",new ColorUIResource(250,235,215));
+            UI.put("Panel.background",new ColorUIResource(250,235,215));
+            JOptionPane.showMessageDialog(null,"Check the next window for the rgb color code", "Info", JOptionPane.INFORMATION_MESSAGE,icon);
+            JOptionPane.showMessageDialog(null,color);
         }
-        else if (ColorInput.equalsIgnoreCase("#FF00FF")){
-            JOptionPane.showMessageDialog(null,"WHAT KIND OF COLOR IS FUCHSIA");
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "An error has occured!", "ERROR!", JOptionPane.ERROR_MESSAGE);
         }
-        else if (ColorInput.equalsIgnoreCase("#E0B0FF")){
-            JOptionPane.showMessageDialog(null,"WHAT KIND OF COLOR IS MAUVE!!!");
-        }
-
     }
 }
